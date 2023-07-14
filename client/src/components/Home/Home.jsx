4207927655a5ec36707dee0355345cac0921b06f
Emmanuel <emanuelnavas@hotmail.com>
2023-07-14 12:48:23 -0300

    updates

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -14,26 +14,26 @@ function Home() {
     const [films, setFilms] = useState(null)
     const [planets, setPlanets] = useState(null)
 
-    useEffect(() => {
-      setCharacters(dataChar)
-      setFilms(dataFilm)
-      setPlanets(dataPlanets)
-    },[])
-    console.log(dataChar)
-    console.log(dataFilm)
-    console.log(dataPlanets)
-  
-    // useEffect(()=> {
-    //   const fetch = async () => {
-    //     const respChars = await axios.get("http://localhost:8000/characters")
-    //     const respFilms = await axios.get("http://localhost:8000/films")
-    //     const respPlanets = await axios.get("http://localhost:8000/planets")
-    //     setCharacters(respChars.data)
-    //     setFilms(respFilms.data)
-    //     setPlanets(respPlanets.data)
-    //   }
-    //   fetch()
+    // useEffect(() => {
+    //   setCharacters(dataChar)
+    //   setFilms(dataFilm)
+    //   setPlanets(dataPlanets)
     // },[])
+    // console.log(dataChar)
+    // console.log(dataFilm)
+    // console.log(dataPlanets)
+  
+    useEffect(()=> {
+      const fetch = async () => {
+        const respChars = await axios.get("http://localhost:8000/characters")
+        const respFilms = await axios.get("http://localhost:8000/films")
+        const respPlanets = await axios.get("http://localhost:8000/planets")
+        setCharacters(respChars.data)
+        setFilms(respFilms.data)
+        setPlanets(respPlanets.data)
+      }
+      fetch()
+    },[])
     
   if(!characters && !films && !planets) return null;
 
--- planets/src/server.js
+++ planets/src/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const server = express();
+
+server.use(cors(
+    {
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+      }
+))
 
 server.use(morgan("dev"));
 server.use(express.json());
